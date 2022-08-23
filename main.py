8cc86c1f5ab91958acae4da8974cc49bb5aca7f4
Atmoslayer <ddsomdim@gmail.com>
2022-08-23 18:39:34 +0300

    Wrap the code in if main

--- main.py
+++ main.py
@@ -1,9 +1,27 @@
+import http.server
+import socketserver
 import datetime
 import pandas
 
 from http.server import HTTPServer, SimpleHTTPRequestHandler
 
 from jinja2 import Environment, FileSystemLoader, select_autoescape
+
+
+PORT = 8080
+
+Handler = http.server.SimpleHTTPRequestHandler
+
+Handler.extensions_map={
+        '.manifest': 'text/cache-manifest',
+  '.html': 'text/html',
+        '.png': 'image/png',
+  '.jpg': 'image/jpg',
+  '.svg':  'image/svg+xml',
+  '.css':  'text/css',
+  '.js':  'application/x-javascript',
+  '': 'application/octet-stream', # Default
+    }
 
 
 def define_year_word_ending(year_number):
@@ -23,33 +41,34 @@ def define_year_word_ending(year_number):
 
     return year_word
 
+if __name__ == '__main__':
+    env = Environment(
+        loader=FileSystemLoader('.'),
+        autoescape=select_autoescape(['html', 'xml'])
+    )
 
-env = Environment(
-    loader=FileSystemLoader('.'),
-    autoescape=select_autoescape(['html', 'xml'])
-)
+    template = env.get_template('template.html')
 
-template = env.get_template('template.html')
+    year_of_foundation = datetime.datetime(year=1920, month=1, day=1, hour=0).year
+    current_year = datetime.datetime.now().year
+    year_number = current_year - year_of_foundation
 
-year_of_foundation = datetime.datetime(year=1920, month=1, day=1, hour=0).year
-current_year = datetime.datetime.now().year
-year_number = current_year - year_of_foundation
-
-excel_data = pandas.read_excel('wine3.xlsx', na_values=' ', keep_default_na=False,
+    excel_data = pandas.read_excel('wine3.xlsx', na_values=' ', keep_default_na=False,
                                    usecols=['Категория', 'Название', 'Сорт', 'Цена', 'Картинка', 'Акция'])
-wines = excel_data.to_dict(orient='records')
+    wines = excel_data.to_dict(orient='records')
 
-formatted_wines = {}
+    formatted_wines = {}
 
-for wine in wines:
-    formatted_wines.setdefault(wine['Категория'], []).append(wine)
+    for wine in wines:
+        formatted_wines.setdefault(wine['Категория'], []).append(wine)
 
-rendered_page = template.render(wines=formatted_wines, year_info=f'Уже {year_number} {define_year_word_ending(year_number)} с вами!')
+    rendered_page = template.render(wines=formatted_wines, year_info=f'Уже {year_number} {define_year_word_ending(year_number)} с вами!')
 
 
-with open('index.html', 'w', encoding="utf8") as file:
-    file.write(rendered_page)
+    with open('index.html', 'w', encoding="utf8") as file:
+        file.write(rendered_page)
 
-server = HTTPServer(('0.0.0.0', 8000), SimpleHTTPRequestHandler)
-server.serve_forever()
+    server = socketserver.TCPServer(("", PORT), Handler)
+    print("serving at port", PORT)
+    server.serve_forever()
 
